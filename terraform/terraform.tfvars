9714e3901ebcd72edbfef61e9c7153826e4efeb8
rei06666 <naruserei@icloud.com>
2025-02-22 21:18:28 +0900

    cognitoの追加

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+system = "kitcom"
+env    = "prd"
+region = "ap-northeast-1"
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "system" {
+  description = "システム名"
+  type        = string
+}
+
+variable "env" {
+  description = "環境名"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
